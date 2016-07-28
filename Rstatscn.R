79012292ce9c3c1951834ddf4acec66690d13620
Wang <U8000108@ten.thomsonreuters.com>
2016-07-28 10:24:43 +0800

    install package with dependencies

---
+++ Rstatscn.R
@@ -0,0 +1,18 @@
+install.packages('rstatscn')
+install.packages("knitr", dependencies = TRUE)
+library(rstatscn)
+library(knitr)
+setwd()
+kable(head(mtcars[, 1:6]))
+statscnDbs()
+statscnQueryZb(dbcode='hgnd')
+statscnQueryZb('A01',dbcode='hgnd')
+
+statscnQueryData('A0101',dbcode='fsnd',moreWd=list(name='reg',value='120000'))
+
+
+set.seed(1213) # for reproducibility of random numbers
+x <- cumsum(rnorm(100))
+plot(x, type = "l", ylab = "$x_{i+1}=x_i+\\epsilon_{i+1}$",xlab = "step")
+
+setwd()
